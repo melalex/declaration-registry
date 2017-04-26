b52511922287679d88bea90e6ba84ffa3a01ed75
Oleksandr Melashchenko <Oleksandr_Melashchenko@epam.com>
2017-04-26 15:45:57 +0300

    Initial commit

---
+++ src/main/java/ua/room414/DemographicRegistryApplication.java
@@ -0,0 +1,12 @@
+package ua.room414;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemographicRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemographicRegistryApplication.class, args);
+	}
+}
